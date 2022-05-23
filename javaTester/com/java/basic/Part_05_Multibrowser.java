de87710067586b54c6a1d8f7980f68c3441acb7d
cnttbklinh93 <cnttbklinh93@gmail.com>
2022-05-23 18:29:58 +0700

    TestNG Annotation priority checked

--- javaTester/com/java/basic/Part_05_Multibrowser.java
+++ javaTester/com/java/basic/Part_05_Multibrowser.java
@@ -20,9 +20,10 @@ public class Part_05_Multibrowser extends AbstractPage{
 	public void beforeTest() {
 		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDriver/chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		
 		System.out.println("Access hrm system");
-		driver.get("https://hrm.anhtester.com/");
+		driver.get("https://hrm.anhtester.com/");	
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
@@ -31,27 +32,56 @@ public class Part_05_Multibrowser extends AbstractPage{
 		driver.quit();
 	}
 
-	@Test(description = "Login to the hrm system")
+	@Test(priority=1)
 	public void Test_01_Login() {
-		System.out.println("Input 'Username'");
+		System.out.println("Login - Step 01: Input 'Username'");
 		waitToElementVisible(driver, "//input[@id='iusername']");
-		sendKeysToElement(driver, "//input[@id='iusername']", "leader01");
+		sendKeysToElement(driver, "//input[@id='iusername']", "admin01");
 		
-		System.out.println("Input 'Password'");
+		System.out.println("Login - Step 02: Input 'Password'");
 		waitToElementVisible(driver, "//input[@id='ipassword']");
 		sendKeysToElement(driver, "//input[@id='ipassword']", "123456");
 		
-		System.out.println("Click to 'Login' button");
+		System.out.println("Login - Step 03: Click to 'Login' button");
 		waitToElementClickable(driver, "//button[@type='submit']");
 		clickToElement(driver, "//button[@type='submit']");
 		
-		System.out.println("Verify Login successfully");
-		waitToElementVisible(driver, "//a[@class='btn btn-smb btn-outline-primary rounded-pill']");		
-		Assert.assertTrue(isControlDisplayed(driver, "//a[@class='btn btn-smb btn-outline-primary rounded-pill']"));
+		System.out.println("Login - Step 04: Verify Login successfully");
+		waitToElementVisible(driver, "//i[@class='feather icon-power']/parent::a");		
+		Assert.assertTrue(isControlDisplayed(driver, "//i[@class='feather icon-power']/parent::a"));
 	}
 	
-	@Test
-	public void Test_01_Verify_Diem_Danh_Menu() {
-
+	
+	@Test(priority=3)
+	public void Test_02_Verify_Helpdesk_Menu()  {
+		System.out.println("Helpdesk Menu - Step 01: Click to 'Helpdesk' menu'");
+		waitToElementClickable(driver, "//span[contains(text(),'Helpdesk')]");
+		clickToElement(driver, "//span[contains(text(),'Helpdesk')]");
+				
+		System.out.println("Helpdesk Menu - Step 02: Verify Helpdesk page is displayed");
+		waitToElementVisible(driver, "//ul[@class='breadcrumb']/li[2]");		
+		Assert.assertEquals(getElementText(driver, "//ul[@class='breadcrumb']/li[2]"),"Helpdesk" );
+	}
+	
+	@Test(priority=2)
+	public void Test_03_test()  {
+		System.out.println("Helpdesk Menu - Step 01: Click to 'Helpdesk' menu'");
+		waitToElementClickable(driver, "//span[contains(text(),'Helpdesk')]");
+		clickToElement(driver, "//span[contains(text(),'Helpdesk')]");
+				
+		System.out.println("Helpdesk Menu - Step 02: Verify Helpdesk page is displayed");
+		waitToElementVisible(driver, "//ul[@class='breadcrumb']/li[2]");		
+		Assert.assertEquals(getElementText(driver, "//ul[@class='breadcrumb']/li[2]"),"Helpdesk" );
+	}
+	
+		@Test(priority=4)
+	public void Test_04_Verify_Logout() {
+		System.out.println("Logout - Step 01: Click to Logout button");
+		waitToElementVisible(driver, "//i[@class='feather icon-power']/parent::a");		
+		clickToElement(driver, "//i[@class='feather icon-power']/parent::a");
+		
+		System.out.println("Logout - Step 02: Verify Login form is displayed");
+		waitToElementVisible(driver, "//form[@class='form-timehrm']");		
+		Assert.assertTrue(isControlDisplayed(driver, "//form[@class='form-timehrm']"));
 	}
 }
